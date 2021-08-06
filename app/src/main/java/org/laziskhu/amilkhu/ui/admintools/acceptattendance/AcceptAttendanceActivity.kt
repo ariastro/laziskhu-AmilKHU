f86eaea20dc29527f54cd9fc950cacc7be7603bc
Ari Astro <ariastronout@gmail.com>
2021-08-07 01:51:19 +0700

    Add accept attendance UI

---
+++ app/src/main/java/org/laziskhu/amilkhu/ui/admintools/acceptattendance/AcceptAttendanceActivity.kt
@@ -0,0 +1,12 @@
+package org.laziskhu.amilkhu.ui.admintools.acceptattendance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.laziskhu.amilkhu.R
+
+class AcceptAttendanceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_accept_attendance)
+    }
+}
